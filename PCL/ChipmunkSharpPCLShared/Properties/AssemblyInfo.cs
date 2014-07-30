f1a8c692c1db3f8a6a4349609868ab81d651ed7f
netonjm <jmedrano@developstudios.com>
2014-07-30 11:41:07 +0200

    Renamed ChipmunkJSSharp->ChipmunkJSSharp no more JS

---
+++ PCL/ChipmunkSharpPCLShared/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle("ChipmunkSharp.PCL.Shared")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCompany("Open Source Software Provided As-Is")]
+[assembly: AssemblyCopyright("Copyright © netonjm")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.2.4.0")]
